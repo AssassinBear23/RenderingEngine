db15678dad27f92eda877384ee0f94e9e18995bc
Beer Huizer <beer.huizer@gmail.com>
2025-10-27 12:19:12 +0100

    Add project files.

---
+++ core/camera.cpp
@@ -0,0 +1,64 @@
+﻿//
+// Created by beer on 13/10/2025.
+//
+
+#include "camera.h"
+
+namespace core {
+    Camera::Camera(const glm::vec3& pos, const glm::vec3& upDir)
+        : position(pos), worldUp(upDir){
+        updateCameraVectors();
+    }
+
+    void Camera::moveForward(const float speed) {
+        position += forward * speed;
+    }
+
+    void Camera::moveBackward(const float speed) {
+        position -= forward * speed;
+    }
+
+    void Camera::moveLeft(const float speed) {
+        position -= right * speed;
+    }
+
+    void Camera::moveRight(const float speed) {
+        position += right * speed;
+    }
+
+    void Camera::moveUp(const float speed) {
+        position += up * speed;
+    }
+
+    void Camera::moveDown(const float speed) {
+        position -= up * speed;
+    }
+
+    void Camera::pivotRotate(const glm::vec2 delta) {
+        yaw   += delta.x * 0.1f;
+        pitch += delta.y * 0.1f;
+        pitch = glm::clamp(pitch, -89.0f, 89.0f);
+        updateCameraVectors();
+    }
+
+    glm::mat4 Camera::getViewMatrix() const {
+        glm::mat4 const view = glm::lookAt(position, position + forward, up);
+        return view;
+    }
+
+    glm::mat4 Camera::getProjectionMatrix(const float width, const float height) const { // NOLINT(*-convert-member-functions-to-static)
+        glm::mat4 const projection = glm::perspective(glm::radians(45.0f), width / height, 0.1f, 100.0f);
+        return projection;
+    }
+
+    void Camera::updateCameraVectors() {
+        glm::vec3 dir;
+        dir.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+        dir.y = sin(glm::radians(pitch));
+        dir.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+
+        forward = glm::normalize(dir);
+        right = glm::normalize(glm::cross(forward, worldUp));
+        up = glm::normalize(glm::cross(right, forward));
+    }
+} // core
---
+++ core/camera.h
@@ -0,0 +1,41 @@
+﻿//
+// Created by beer on 13/10/2025.
+//
+
+#ifndef RAWENGINE_CAMERA_H
+#define RAWENGINE_CAMERA_H
+
+#pragma once
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace core {
+    class Camera {
+    public:
+        glm::vec3 position;
+        glm::vec3 worldUp;
+        glm::vec3 forward{};
+        glm::vec3 right{};
+        glm::vec3 up{};
+        float yaw = -90.0f;
+        float pitch = 0.0f;
+
+        Camera(const glm::vec3& pos, const glm::vec3& upDir);
+
+        void moveForward(float speed);
+        void moveBackward(float speed);
+        void moveLeft(float speed);
+        void moveRight(float speed);
+        void moveUp(float speed);
+        void moveDown(float speed);
+        void pivotRotate(glm::vec2 delta);
+
+        glm::mat4 getViewMatrix() const;
+        glm::mat4 getProjectionMatrix(float width, float height) const;
+
+    private:
+        void updateCameraVectors();
+    };
+}
+
+#endif //RAWENGINE_CAMERA_H
